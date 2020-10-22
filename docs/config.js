440bb479383106590878ddc6af14e4efcde627d4
tingyun <tingyun@pinduoduo.com>
2020-10-22 15:40:43 +0800

    fix: 添加中文路径支持

--- docs/config.js
+++ docs/config.js
@@ -1,6 +1,7 @@
 export default {
   title: 'docbook',
   logo: 'Docbook',
+  port: 4000,
   github: "https://github.com/fantasticit/docbook",
   js: ["https://s2.ssl.qhres.com/!f868948f/vue.min.js"],
   navs: [
